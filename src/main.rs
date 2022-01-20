51c1d33ab909343c56dc86faf1dcb0bf940c09aa
nic ferrier <nic@ferrier.me.uk>
2022-01-20 01:10:03 +0000

    added number sequence function num

--- src/main.rs
+++ src/main.rs
@@ -240,6 +240,35 @@ fn default_env<'a>() -> RispEnv<'a> {
     )
   );
   data.insert(
+    "num".to_string(),
+    RispExp::Func(
+      |args: &[RispExp]| -> Result<RispExp, RispErr> {
+	if args.len() < 1 {
+	  return Err(RispErr::Reason("pass a max value".to_string()));
+	}
+
+	let max = match args[0] {
+	  RispExp::Number(x) => x as i64,
+	  _ => return Err(RispErr::Reason("arg is not a number".to_string())),
+	};
+
+	let start = if args.len() < 2 { 0 } else {
+	  match args[1] {
+	    RispExp::Number(x) => x as i64,
+	    _ => return Err(RispErr::Reason("arg is not a number".to_string())),
+	  }
+	};
+
+	let mut res: Vec<RispExp> = Vec::new();
+	let r = start..max;
+	for m in r {
+	  res.push(RispExp::Number(m as f64));
+	}
+	Ok(RispExp::List(res))
+      }
+    )
+  );
+  data.insert(
     "+".to_string(), 
     RispExp::Func(
       |args: &[RispExp]| -> Result<RispExp, RispErr> {
